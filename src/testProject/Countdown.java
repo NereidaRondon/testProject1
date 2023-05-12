c4f253fdf42e355c28731cac375d017fbb0456a5
nereida-promineo <nereida.promineo@gmail.com>
2023-05-12 09:33:05 -0400

    first commit

---
+++ src/testProject/Countdown.java
@@ -0,0 +1,16 @@
+package testProject;
+
+public class Countdown {
+
+	public static void main(String[] args) {
+		
+		// counts down to 0 from 10
+		for(int i = 10; i >= 0; i--) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Launch!");
+
+	}
+
+}
