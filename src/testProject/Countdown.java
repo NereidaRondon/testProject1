2ab3394c872512676be32ec38b40278adbc96137
nereida-promineo <nereida.promineo@gmail.com>
2023-05-12 09:38:28 -0400

    edit to loop

--- src/testProject/Countdown.java
+++ src/testProject/Countdown.java
@@ -5,7 +5,7 @@ public class Countdown {
 	public static void main(String[] args) {
 		
 		// counts down to 0 from 10
-		for(int i = 10; i >= 0; i--) {
+		for(int i = 15; i >= 0; i--) {
 			System.out.println(i);
 		}
 		
